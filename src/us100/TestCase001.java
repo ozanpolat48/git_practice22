cd1d8337f121ba3cc8524be91696f615c0725ccf
ozanpolat48 <mrozanpolat@gmail.com>
2022-07-19 22:25:10 +0300

    started Test Case 002

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -4,7 +4,7 @@ public class TestCase001 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");   // sol taraf neden yesil oluyor birşeyler yapınca ? nedeni hey ozan sen yeni birşeyler yapıyorsun demek.
+        System.out.println("Hello");   // sol taraf neden yesil oluyor birşeyler yapınca ? nedeni hey ozan sen yeni birşeyler yapıyorsun demek. Sonra commit yapınca geçiyor.
 
     }
 }
